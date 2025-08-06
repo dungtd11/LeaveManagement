aaf720d42db9b1741e27223355c51246ea408b4d
TRAN DOAN DUNG(Tran Doan Dung, Eric) <dungtd@lgcns.com>
2025-08-06 14:00:21 +0700

    6-46

---
+++ HR.LeaveManagemnt.Application/Features/LeaveAllocation/Queries/GetLeaveAllocationDetails/GetLeaveAllocationDetailQuery.cs
@@ -0,0 +1,10 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetailsDto;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails
+{
+    public class GetLeaveAllocationDetailQuery : IRequest<LeaveAllocationDetailsDto>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ HR.LeaveManagemnt.Application/Features/LeaveAllocation/Queries/GetLeaveAllocationDetails/LeaveAllocationDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagemnt.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetailsDto
+{
+    public class LeaveAllocationDetailsDto
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+        public LeaveTypeDto LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
