6b8f03ce3a2abfffd44a915ec5aa8886e8a2283e
TRAN DOAN DUNG(Tran Doan Dung, Eric) <dungtd@lgcns.com>
2025-08-11 15:19:02 +0700

    6-49

---
+++ HR.LeaveManagemnt.Application/Features/LeaveRequest/Queries/GetLeaveRequestDetail/GetLeaveRequestDetailQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using HR.LeaveManagemnt.Application.Contracts.Persistence;
+using HR.LeaveManagemnt.Application.Exceptions;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestDetail
+{
+    public class GetLeaveRequestDetailQueryHandler : IRequestHandler<GetLeaveRequestDetailQuery, LeaveRequestDetailsDto>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+        //private readonly IUserService _userService;
+
+        public GetLeaveRequestDetailQueryHandler(ILeaveRequestRepository leaveRequestRepository,
+            IMapper mapper
+            //,IUserService userService
+            )
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+            //this._userService = userService;
+        }
+        public async Task<LeaveRequestDetailsDto> Handle(GetLeaveRequestDetailQuery request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = _mapper.Map<LeaveRequestDetailsDto>(await _leaveRequestRepository.GetLeaveRequestWithDetails(request.Id));
+
+            if (leaveRequest == null)
+                throw new NotFoundException(nameof(LeaveRequest), request.Id);
+
+            // Add Employee details as needed
+            //leaveRequest.Employee = await _userService.GetEmployee(leaveRequest.RequestingEmployeeId);
+
+            return leaveRequest;
+        }
+    }
+}
